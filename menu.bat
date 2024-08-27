28318bc8e6b88f4f9d87b301a61a228d6bf53698
empo <158759842+empogithub@users.noreply.github.com>
2024-08-27 17:34:47 +0200

    Add files via upload

---
+++ menu.bat
@@ -0,0 +1,286 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCuDJH+t2GcDFB9bWwqOM2q+FYkb76izvqPJq04SNA==
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdFe5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJhZks0
+::ZQ05rAF9IBncCkqN+0xwdVsFAlTi
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWBnE8U8gJxxdWAyXPgs=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCuDJH+t2GcDFB9bWwqOM2q+FYkX7fH86uTe+i0=
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+goto menu
+:menu
+cls
+echo.
+echo     _________
+echo.
+echo     keygen98
+echo.
+echo     _________
+echo.
+echo.
+echo  [1] Start
+echo  [2] Help
+echo  [3] About
+echo  [4] Exit
+echo.
+
+set /p selection=
+
+if %selection%==1 goto 1g
+if %selection%==2 goto  g
+if %selection%==3 goto  g
+if %selection%== goto  g
+if %selection%== goto  g
+if %selection%== goto  g
+if %selection%==4 exit
+goto menu
+
+:1g
+cls
+echo.
+echo.
+echo.   Note!!! win nt - xp only keys work if you download from https://dl.malwarewatch.org/windows/
+echo     _________
+echo.
+echo     keygen98
+echo.
+echo     _________
+echo.
+echo     Choose a oprating system
+echo  [1] win 98
+echo  [2] win 95
+echo  [3] win me 
+echo  [4] win 2000
+echo  [5] win nt 4
+echo  [6] win xp
+echo  [7] win 8
+echo  [8] win 10
+echo  [9] win 11
+echo.
+echo  [10] back
+set /p selection=
+
+if %selection%==1 goto  98
+if %selection%==2 goto  95
+if %selection%==3 goto  me
+if %selection%==4 goto  2000
+if %selection%==5 goto  nt4
+if %selection%==6 goto  xp
+if %selection%==7 goto 8
+if %selection%==8 goto 10
+if %selection%==7 goto 11
+if %selection%==10 goto menu
+goto 1g
+
+:98
+cls
+echo   key: RW9MG-QR4G3-2WRR9-TG7BH-33GXB
+echo.
+echo [1] copy to clipbord
+echo [2] back
+set /p selection=
+
+if %selection%==1 echo RW9MG-QR4G3-2WRR9-TG7BH-33GXB| clip
+if %selection%==2 goto  1g
+
+:95
+cls
+echo   key: 24796-OEM-0014736-66386
+echo.
+echo [1] copy to clipbord
+echo [2] back
+set /p selection=
+
+if %selection%==1 echo 24796-OEM-0014736-66386| clip
+if %selection%==2 goto  1g
+
+:me
+cls
+echo   key: B6BYC-6T7C3-4PXRW-2XKWB-GYV33
+echo.
+echo [1] copy to clipbord
+echo [2] back
+set /p selection=
+
+if %selection%==1 echo B6BYC-6T7C3-4PXRW-2XKWB-GYV33| clip
+if %selection%==2 goto  1g
+
+:2000
+cls
+echo   key: G7VKM-WM72Y-JJXGT-F4BJG-D6RPQ
+echo [1] copy to clipbord
+echo [2] back
+set /p selection=
+
+if %selection%==1 echo G7VKM-WM72Y-JJXGT-F4BJG-D6RPQ| clip
+if %selection%==2 goto  1g
+
+:nt4
+cls
+echo   choose a key
+echo.
+echo [1] key 1
+echo [2] key 2
+echo [3] key 3
+echo [4] key 4
+echo [5] back
+echo.
+set /p selection=
+
+if %selection%==1 goto nt1k
+if %selection%==2 goto  nt2k
+if %selection%==3 goto  nt3k
+if %selection%==4 goto  nt4k
+if %selection%==5 goto  1g
+
+:nt1k
+cls
+echo   key: 28997-OEM-0025957-49297
+echo.
+echo [1] copy to clipbord
+echo [2] back
+set /p selection=
+
+if %selection%==1 echo 28997-OEM-0025957-49297| clip
+if %selection%==2 goto  1g
+
+:nt2k
+cls
+echo   key: 30495-OEM-0006276-08104
+echo.
+echo [1] copy to clipbord
+echo [2] back
+set /p selection=
+
+if %selection%==1 echo 30495-OEM-0006276-08104| clip
+if %selection%==2 goto  1g
+
+:nt3k
+cls
+echo  key: 40159-OEM-0211111-11111
+echo.
+echo [1] copy to clipbord
+echo [2] back
+set /p selection=
+
+if %selection%==1 echo 40159-OEM-0211111-11111| clip
+if %selection%==2 goto  1g
+
+:nt4k
+cls
+echo   key: 34698-OEM-0039682-50656
+echo.
+echo [1] copy to clipbord
+echo [2] back
+set /p selection=
+
+if %selection%==1 echo 34698-OEM-0039682-50656| clip
+if %selection%==2 goto  1g
+
+:xp
+cls
+echo   choose version
+echo.
+echo [1] x64
+echo [2] x86
+echo [3] back
+set /p selection=
+
+if %selection%==1 goto 64xp
+if %selection%==2 goto  86xp
+if %selection%==3 goto 1g
+
+:64xp
+cls
+echo   key: VCFQD-V9FX9-46WVH-K3CD4-4J3JM
+echo.
+echo [1] copy to clipbord
+echo [2] back
+set /p selection=
+
+if %selection%==1 echo VCFQD-V9FX9-46WVH-K3CD4-4J3JM| clip
+if %selection%==2 goto  1g
+
+:86xp
+cls
+echo   key: MRX3F-47B9T-2487J-KWKMF-RPWBY
+echo.
+echo [1] copy to clipbord
+echo [2] back
+set /p selection=
+
+if %selection%==1 echo MRX3F-47B9T-2487J-KWKMF-RPWBY| clip
+if %selection%==2 goto  1g
+
+:8
+cls
+echo   start activation?
+echo.
+echo [1] start activation
+echo [2] back
+set /p selection=
+
+if %selection%==1 goto batch8
+if %selection%==2 goto  1g
+goto 8
+
+:batch8
+cd files
+start 8.bat
+exit
+:10
+cls
+echo   start activation?
+echo.
+echo [1] start 
+echo [2] back
+set /p selection=
+
+if %selection%==1 goto batch10
+if %selection%==2 goto  1g
+
+:batch10
+cd files
+start 10.bat
+exit
+
+:11
+cls
+echo   start activation?
+echo.
+echo [1] start 
+echo [2] back
+set /p selection=
+
+if %selection%==1 goto batch11
+if %selection%==2 goto  1g
+
+:batch11
+cd files
+start 11.bat
+exit
